3a94317be1c506d04fead19aee36d3c30cca0b72
Sergiy Leshchenko <sergrut99@gmail.com>
2022-08-05 18:35:37 +0300

    Create ICommandDispatcher and implement it

---
+++ CQRS-ES/CQRS.Core/Infrastructure/ICommandDispatcher.cs
@@ -0,0 +1,11 @@
+﻿using CQRS.Core.Commands;
+
+namespace CQRS.Core.Infrastructure
+{
+    public interface ICommandDispatcher
+    {
+        bool RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand;
+
+        Task SendAsync(BaseCommand command);
+    }
+}
---
+++ SM-POST/Post.Cmd/Post.Cmd.Infrastructure/Dispatchers/CommandDispatcher.cs
@@ -0,0 +1,34 @@
+﻿using CQRS.Core.Commands;
+using CQRS.Core.Infrastructure;
+
+namespace Post.Cmd.Infrastructure.Dispatchers
+{
+    public class CommandDispatcher : ICommandDispatcher
+    {
+        private readonly Dictionary<Type, Func<BaseCommand, Task>> _handlers = new();
+
+        public bool RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand
+        {
+            if (_handlers.ContainsKey(typeof(T)))
+            {
+                return false;
+            }
+
+            _handlers.Add(typeof(T), c => handler((T)c));
+
+            return true;
+        }
+
+        public async Task SendAsync(BaseCommand command)
+        {
+            if (_handlers.TryGetValue(command.GetType(), out Func<BaseCommand, Task>? handler))
+            {
+                await handler(command);
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(handler), "No command handler was registered.");
+            }
+        }
+    }
+}
