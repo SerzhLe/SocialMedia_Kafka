bb371c0df6ed476eb49002596c2e37944ba022cf
Sergiy Leshchenko <sergrut99@gmail.com>
2022-08-09 11:42:34 +0300

    Create entities - post and comment, define repository interfaces for these entities

---
+++ SM-POST/Post.Query/Post.Query.Domain/Entities/CommentEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Post.Query.Domain.Entities
+{
+    [Table("Comments")]
+    public class CommentEntity
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public bool Edited { get; set; }
+
+        public Guid PostId { get; set; }
+
+        [JsonIgnore]
+        public PostEntity Post { get; set; }
+    }
+}
---
+++ SM-POST/Post.Query/Post.Query.Domain/Entities/PostEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Post.Query.Domain.Entities
+{
+    [Table("Posts")]
+    public class PostEntity
+    {
+        public Guid Id { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime DatePosted { get; set; }
+
+        public string Message { get; set; }
+
+        public int Likes { get; set; }
+
+        public ICollection<CommentEntity> Comments { get; set; }
+    }
+}
